de63ce436a99e1bc08c7fd717935b5b88fea0dc7
MFK <>
2022-09-10 19:32:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hi, My name is Bot, I am here to chat with you")
+
+name = input (" What is your name?")
+if name =="Sam":
+  print("What a good name!")
+print("Hi",name)
+how = input ("How do you feel today? ")
+print("I am feeling "+ how + " too")
+age1 = int(input("How old are you?"))
+print(str(age1)," is a good age to live the life.")
+print("I am 15, but I will be 16 in October")
+Q1 = input ("What is your favorite color ?")
+print(Q1 + " is my favorite color too")
+Q2 = input (" Where are you from? ")
+print("Wow!" + " I am also from " + Q2 )
+father = input ("What is your father's name? ")
+if father =="Kevin":
+  print(" Oh, I have a friend named Kevin")
+  print("We might be relatives, hahaha!")
+print("What a cool name!")
+end = input("Do you you have anything else to say")
+if end == "yes":
+  print("I am here to talk to you")
+else :
+  exit()
+
+
+ 
+
+
+
+
+
